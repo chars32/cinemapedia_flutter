19aa08dcf5caed9d86bb6a1e922a5722e304506f
Frontend <frontend@healonsite.mx>
2023-07-27 14:39:50 -0600

    router y primer home_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:cinemapedia/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+
+import 'package:cinemapedia/config/router/app_router.dart';
+import 'package:cinemapedia/config/theme/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,14 +12,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
     );
   }
 }
