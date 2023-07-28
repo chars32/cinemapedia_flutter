0da571e257bbfba93145f0139517e0e9fa8d38d8
chars32 <chars24@gmail.com>
2023-07-27 22:58:14 -0600

    archivo de barril

--- lib/presentation/providers/movies/movies_providers.dart
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -3,11 +3,13 @@ import 'package:cinemapedia/presentation/providers/movies/movies_repository_prov
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final nowPlayingMoviesProvider =
-    StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
-  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
+    StateNotifierProvider<MoviesNotifier, List<Movie>>(
+  (ref) {
+    final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
 
-  return MoviesNotifier(fetchMoreMovies: fetchMoreMovies);
-});
+    return MoviesNotifier(fetchMoreMovies: fetchMoreMovies);
+  },
+);
 
 typedef MovieCallback = Future<List<Movie>> Function({int page});
 
