3731f30db5a04267eb615a49a28fd3a142468fdf
chars32 <chars24@gmail.com>
2023-07-31 21:44:17 -0600

    Repository y Provider - Implementacion

---
+++ lib/presentation/providers/actors/actors_repository_provider.dart
@@ -0,0 +1,8 @@
+import 'package:cinemapedia/infrastructure/datasources/actor_moviedb_datasources.dart';
+import 'package:cinemapedia/infrastructure/repositories/actor_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Repositorio inmutable (Provider)
+final actorsRepositoryProvider = Provider((ref) {
+  return ActorRepositoryImpl(ActorMovieDbDatasource());
+});
