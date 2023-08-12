c2d7c4d526d5fc768f57aa8917b662b9f93ddc9a
chars32 <chars24@gmail.com>
2023-08-12 16:50:32 -0600

    Isar Datasource Implementation

--- lib/infrastructure/datasources/isar_datasource.dart
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -1,7 +1,28 @@
 import 'package:cinemapedia/domain/datasource/local_storage_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class IsarDatasource extends LocalStorageDataSource {
+  late Future<Isar> db;
+
+  IsarDatasource() {
+    db = openDB();
+  }
+
+  Future<Isar> openDB() async {
+    final dir = await getApplicationDocumentsDirectory();
+
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open(
+        [MovieSchema],
+        directory: dir.path,
+        inspector: true,
+      );
+    }
+    return Future.value(Isar.getInstance());
+  }
+
   @override
   Future<bool> isMovieFavorite(int movieId) {
     // TODO: implement isMovieFavorite
@@ -9,14 +30,14 @@ class IsarDatasource extends LocalStorageDataSource {
   }
 
   @override
-  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
-    // TODO: implement loadMovies
+  Future<void> toggleFavorite(Movie movie) {
+    // TODO: implement toggleFavorite
     throw UnimplementedError();
   }
 
   @override
-  Future<void> toggleFavorite(Movie movie) {
-    // TODO: implement toggleFavorite
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
+    // TODO: implement loadMovies
     throw UnimplementedError();
   }
 }
